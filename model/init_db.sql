cc4284e7dfa87277e160b9014fbc41d29a3b5910
Carrie Hancock <carrie.a.hancockgmail.com>
2023-03-03 18:41:26 +0000

    initial table

--- model/init_db.sql
+++ model/init_db.sql
@@ -1,8 +1,23 @@
-DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS characters;
 
-CREATE TABLE recipes (
+CREATE TABLE characters (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(100) NOT NULL,
-    ingredients VARCHAR(255) NOT NULL
+    names VARCHAR(100) NOT NULL,
+    biography VARCHAR(500) NOT NULL,
+    motive VARCHAR(500) NOT NULL
 );
+ INSERT INTO characters (names, biography, motive
+ ) VALUES (
+    "Holly",
+    "Lives in London and has a pet dog",
+    "The person stole her dog's favourite toy"
+ );
+
+ INSERT INTO characters (names, biography, motive
+ ) VALUES (
+    "Marina",
+    "Lives in Wigan and wishes for Brazilian weather",
+    "The person cast a spell to make the sky permanently grey"
+ )
+
 
