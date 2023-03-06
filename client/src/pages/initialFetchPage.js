d3ba8435c548f341ea5bf9e05245b1c10dce6f8f
Carrie Hancock <carrie.a.hancockgmail.com>
2023-03-06 17:30:11 +0000

    table edits

---
+++ client/src/pages/initialFetchPage.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+const gameFetch = () => {
+  const [characters, setEntries] = useState([]);
+  
+
+  useEffect(() => {
+    const fetchEntries = async () => {
+      const response = await fetch('http://localhost:5050/characters')
+      const mountains = await response.json();
+
+      setEntries(mountains)
+    }
+
+    fetchEntries();
+  }, []);
+
+  const scotlandMountains = mountains.filter(mountain => mountain.country === "Scotland") 
+
+  const englandMountains = mountains.filter(mountain => mountain.country === "England")
