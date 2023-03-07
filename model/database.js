f0b1914375bda0c778b5683eee93ef50c21e3f2f
Carrie Hancock <carrie.a.hancockgmail.com>
2023-03-07 17:03:19 +0000

    fully populated database

--- model/database.js
+++ model/database.js
@@ -8,26 +8,25 @@ const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 
 const con = mysql.createConnection({
-  host: DB_HOST || "localhost",
+  host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
-  password: DB_PASS || "Mattbellamy7",
-  database: DB_NAME || "recipes",
+  password: DB_PASS || "CodeOp2022",
+  database: DB_NAME || "Code_Quiz",
   multipleStatements: true
 });
 
-con.connect(function(err) {
+let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
+
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 
-  let sql = fs.readFileSync(__dirname+"/init_db.sql").toString();
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table creation `items` was successful!");
+    console.log("Table creation was successful!");
 
     console.log("Closing...");
   });
 
   con.end();
 });
-
-
