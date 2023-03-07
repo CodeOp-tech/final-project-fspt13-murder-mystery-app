9dea2d294352455c2321427c541185e3a6120941
Marina <marinapcoimbra@gmail.com>
2023-03-07 10:05:03 +0000

    pictures and suspects added

--- client/src/pages/pages/Suspects.js
+++ client/src/pages/pages/Suspects.js
@@ -4,17 +4,50 @@ export default function Suspects() {
   const router = useRouter();
   return (
     <>
-      <div className="suspects">
+      <div className="suspects_container">
         <h1>Suspects</h1>
-        <ol>
-          <li>
-            1. Emma Johnson - Robert's former assistant who was fired from her
-            job after she was caught embezzling money from the company. She felt
-            wronged by Robert and believed he deserved to die. Date of
-            birth:15/06/1983 Dominant hand: Right-handed Smoker? No Hobbies:
-            Crafting, hiking, dancing Do you own a weapon? No
-          </li>
-        </ol>
+        <div className="suspects">
+          1. Emma Johnson - Robert's former assistant who was fired from her job
+          after she was caught embezzling money from the company. She felt
+          wronged by Robert and believed he deserved to die.
+          <br></br> Date of birth:15/06/1983 <br></br>
+          Dominant hand: Right-handed <br></br>
+          Smoker? No
+          <br></br>
+          Hobbies: Crafting, hiking, dancing Do you own a weapon? No
+          <br></br>
+          <br></br>
+          2. Jack Wilson - A rival businessman who was envious of Robert's
+          success and saw him as a threat to his own business.<br></br>
+          He believed that killing Robert would eliminate his competition.
+          <br></br>
+          Date of birth:30/12/1978<br></br>
+          Dominant hand: Right-handed<br></br>
+          Smoker? Yes<br></br>
+          Do you own a weapon? Yes Hobbies: Sailing, Skiing, collecting stamps
+          <br></br>
+          <br></br>
+          3. Karen Thompson - Robert's wife who was unhappy in their marriage
+          and wanted to collect his life insurance policy.<br></br>
+          She saw his death as a way to get what she wanted.<br></br>
+          Date of birth: 21/02/1964<br></br>
+          Smoker?Yes<br></br>
+          Do you own a weapon? No<br></br>
+          Dominant hand: Left handed<br></br>
+          Hobbies: reading, gardening, astrology<br></br>
+          <br></br>
+          <br></br>
+          4. Tom Davis - Robert's friend and business partner who was struggling
+          financially and owed a lot of money to Robert.<br></br>
+          He saw killing Robert as a way to solve his financial problems.
+          <br></br>
+          Date of birth: 17/05/1965<br></br>
+          Dominant hand: right-handed<br></br>
+          Smoker? Yes<br></br>
+          Do you own a weapon? No<br></br>
+          Hobbies: listen to classical music, bet on horses, woodturning
+          <br></br>
+        </div>
 
         <button type="button" onClick={() => router.push("/quiz")}>
           Click Me
