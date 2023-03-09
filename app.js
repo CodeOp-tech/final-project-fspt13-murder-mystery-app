f497dddd86adddfd3d1378c456c5597bee64aa99
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-03-09 21:56:56 +0100

    add cors and fix package.json file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var cors = require('cors');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -11,7 +11,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 
-// app.use(cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
