17bd37cb2a60fa9858a5a575d737aaf77b1db0f2
Marina <marinapcoimbra@gmail.com>
2023-03-11 10:58:21 +0000

    page changes

--- client/src/pages/pages/index.js
+++ client/src/pages/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         <button type="button" onClick={() => router.push("/pages/cases")}>
           Click Me
         </button>
-
-     </>
-    )
-    }
+      </div>
+    </>
+  );
+}
