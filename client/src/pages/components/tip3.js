bb2a1f6e04b282dc4a5b57e4d759ca8cb4f41d5e
Holly <hollycmanganoni@gmail.com>
2023-03-14 18:21:03 +0000

    Added popup components

---
+++ client/src/pages/components/tip3.js
@@ -0,0 +1,12 @@
+export default function Tip3 () {
+    <>
+    <div className="popup">
+        <div className="popup-content">
+            <span className="close">&times;</span>
+            <p>I'm a popup</p>
+        </div>
+    </div>
+    
+    
+    </>
+}
---
+++ client/src/pages/components/tip5.js
@@ -0,0 +1,13 @@
+export default function Tip5 () {
+    <>
+    <div className="popup">
+        <div className="popup-content">
+            <span className="close">&times;</span>
+            <p>Police found that by the phone location 
+            Tom Davies was at the crime scene when the crime was committed.</p>
+        </div>
+    </div>
+    
+    
+    </>
+}
